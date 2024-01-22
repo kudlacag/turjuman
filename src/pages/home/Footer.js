cce9bd72381f8bbe8d1a0a8da859e9c2cdb8b022
unknown <kudlacag114@yahoo.com>
2024-01-23 00:21:37 +0100

    add gh-pages

--- src/pages/home/Footer.js
+++ src/pages/home/Footer.js
@@ -11,8 +11,8 @@ function Footer() {
    {/* card */}
    <div class="card bg-dark text-white text-center">
     <div className="card-body">
-      <h4 className="card-title">More Information</h4>
-      <p className="card-text">Some example text. Some example text.</p>
+      <h4 className="card-title">Somalische Dolmetscher</h4>
+      <p className="card-text">Beratung über Asylverfahren</p>
       ©2024 Copyright:  <a class="text-body"  target="_blank" href="https://linkedin.com/in/ramadan-hussein-ahmed-5125a521a" className="card-link text-info">Ramadan Ahmed</a>
       {/* <a href="#" className="card-link">Another link</a> */}
     </div>

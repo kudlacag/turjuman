5ae0e51ce6cd1fd8cbe6866e550bb78fb4b7e7b4
unknown <kudlacag114@yahoo.com>
2024-01-23 02:54:34 +0100

    deployment 3

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ import Services from './pages/links/Services';
 import About from './pages/links/About';
 import Footer from './pages/home/Footer';
 import Contacts from './pages/links/Contacts';
+
 function App() {
   return (
     <div className="App">
     <Header />
 
-    <Home />
    <Routes>
 
-    {/* <Route path='/' element={<Home /> } /> */}
+    <Route path='*' element={<Home /> } />
+
+
     <Route path='/services' element={ <Services />} />
     <Route path='/about' element={ <About />} />
     <Route path='/contacts' element={ <Contacts />} />
--- src/pages/home/Landing.js
+++ src/pages/home/Landing.js
@@ -8,7 +8,7 @@ function Landing() {
         Herzlich willkommen!
         </h1>
         <p className="text-center my-5 display-6 p1-1 ">
-        Sie benötigen professionelle Dolmetscherdienste für die somalische Sprache?
+        Sie sind auf der Suche nach professionellen Dolmetscherdiensten im Raum München?
         </p>
       </div>
     </div>

8ba135672e7eba8dc853d53f3a0fc7e7933c58a4
unknown <kudlacag114@yahoo.com>
2024-01-23 01:59:00 +0100

    deployment 3

--- src/App.js
+++ src/App.js
@@ -12,11 +12,14 @@ function App() {
     <div className="App">
     <Header />
 
+    <Home />
    <Routes>
-    <Route path='/' element={<Home /> } />
+
+    {/* <Route path='/' element={<Home /> } /> */}
     <Route path='/services' element={ <Services />} />
     <Route path='/about' element={ <About />} />
     <Route path='/contacts' element={ <Contacts />} />
+
    </Routes>
    <Footer />
     </div>
